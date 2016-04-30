f9455566116e51f95a047c55f1404e23d3eb0926
Benjamin Rosner <brosner@barnard.edu>
2016-04-29 20:49:00 -0400

    Updated to allow for more than one record.

--- dynamic_dns_updater_for_namesilo.com.py
+++ dynamic_dns_updater_for_namesilo.com.py
@@ -1,25 +1,45 @@
-import requests
 import sys
 import xml.etree.ElementTree as eltree
+
+import requests
 
 # NameSilo API Dynamic DNS
 # Original Concept and Credits go to:
 # http://www.forkrobotics.com/2014/10/dynamic-dns-with-namesilo-and-powershell/
 
 # Variables
-api_key = "xxxxxxxx"
+api_key = "XXXXXXXXXXXXXXXXXXXXXX"
 domain = "r-ben.com"
-record = "freedom"
+records_fqdn = ["r-ben.com", "freedom.r-ben.com"]
+record_type = "A"
 
-## Code - Do not edit below this line
+######################################
+# Code - Do not edit below this line #
+######################################
 namesilo_api = 'https://www.namesilo.com/api/'
 version_param = '?version=1'
 resp_type_param = '&type=xml'
 api_key_param = '&key=' + api_key
 domain_param = '&domain=' + domain
-record_param = '&rrhost=' + record
-record_fqdn = record + '.' + domain
+# record_fqdn = record + '.' + domain if record else domain
 record_ttl = '&rrttl=3600'
+
+
+def update_records():
+    print('Record name: ' + record_host + '\nRecord\'s IP: ' + record_ip)
+    if callers_ip != record_ip:
+        print('Update required.')
+        record_id_param = '&rrid=' + record_id
+        record_ip_param = '&rrvalue=' + callers_ip
+        update_record = ''.join(
+            [namesilo_api, 'dnsUpdateRecord', version_param, resp_type_param, api_key_param, domain_param, record_host,
+             record_id_param, record_ip_param, record_ttl])
+        response = webworker.get(update_record)
+        print(response.text)
+        # Some validation should be done on response - I think they return 300 if success. Too lazy.
+    else:
+        print('No update was required.')
+
 
 # Setup webworker
 webworker = requests.session()
@@ -37,27 +57,16 @@ parsed = eltree.fromstring(records_list.text)
 
 # Set our IP address from the response.
 callers_ip = parsed.find('.//ip').text
+print('Current IP: ' + callers_ip)
 
-# Gather all of our resource_records
+# Gather all of our resource_records.
 resource_records = parsed.findall('.//resource_record')
 
 # This could be done plenty of other ways, but I like this for ease of readability.
 # Find our FQDN's record_id.
 for record in resource_records:
-    if (record[2].text == record_fqdn):
+    if (record[2].text in records_fqdn) and (record[1].text == record_type):
+        record_host = record[2].text
         record_id = record[0].text
         record_ip = record[3].text
-        continue
-
-print('Current IP: ' + callers_ip + '\nRecord\'s IP: ' + record_ip)
-if (callers_ip != record_ip):
-    print('Update required.')
-    record_id_param = '&rrid=' + record_id
-    record_ip_param = '&rrvalue=' + callers_ip
-    update_record = ''.join(
-        [namesilo_api, 'dnsUpdateRecord', version_param, resp_type_param, api_key_param, domain_param, record_param,
-         record_id_param, record_ip_param, record_ttl])
-    response = webworker.get(update_record)
-    # Some validation should be done on response - I think they return 300 if success. Too lazy.
-else:
-    print('No update was required.')
+        update_records()
